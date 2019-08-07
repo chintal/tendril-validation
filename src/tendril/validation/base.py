354cbdddc2ab635d951e29f634a564273020120b
Chintalagiri Shashank <shashank.chintalagiri@gmail.com>
2019-08-07 23:36:42 +0530

    Migrated to the new format

--- src/tendril/validation/base.py
+++ src/tendril/validation/base.py
@@ -18,6 +18,10 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+"""
+Tendril Validation Base (:mod:`tendril.validation.base`)
+========================================================
+"""
 
 from copy import copy
 from colorama import Style
